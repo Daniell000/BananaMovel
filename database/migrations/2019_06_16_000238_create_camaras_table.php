82c3df739200685c60f172b9f463e58ca869acc9
Daniell000 <51684612+Daniell000@users.noreply.github.com>
2019-06-15 22:18:55 -0300

    Criação da tabela e CRUD das reservas

---
+++ database/migrations/2019_06_16_000238_create_camaras_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCamarasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camaras', function (Blueprint $table) {
+            $table->increments('id');
+			$table->smallInteger('lote')->unique(); //Lote é id único
+			$table->char('identificacao', 10)->unique(); //Além dos nomes das câmaras terem largura fixa, uma câmara não pode ter dois usos ao mesmo tempo
+            $table->string('tipo', 20);
+			$table->tinyInteger('temperatura');
+			$table->string('tempoEsperado', 255);
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camaras');
+    }
+}
